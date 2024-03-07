fbc2a7008406627f3b5f58d9e6611f15d80293cc
Manyzeur <hugo-machado@outlook.fr>
2024-03-07 16:40:25 +0100

    [SCORE]
    - score ui finished

--- Assets/Script/Scores/ScoreManager.cs
+++ Assets/Script/Scores/ScoreManager.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 using TMPro;
 
 public class ScoreManager : Singleton<ScoreManager>
-{
-    [SerializeField] private int _score = 0;
-    [SerializeField] private TextMeshProUGUI _text = null; //mettre dans le Score controller ? 
+{ 
     public float ComputeScore()
     {
             return CharacterManager.Instance.GoodThinking.Count;
     }
-
-    public void newscore()
-    {
-        if (GameManager.Instance.CurrentGamePhase == EGamePhase.REPOS)
-            _text.text = ScoreManager.Instance.ComputeScore();
-    }
-
     //ScoreManager.Instance.ComputeScore();
 
 }
---
+++ Assets/Script/Scores/ScoreUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI _text = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.OnChangePhase += NewScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        GameManager.Instance.OnChangePhase -= NewScore;
+    }
+
+    void OnApplicationQuit()
+    {
+        GameManager.Instance.OnChangePhase -= NewScore;
+    }
+
+    public void NewScore()
+    {
+    if (GameManager.Instance.CurrentGamePhase == EGamePhase.REPOS)
+    _text.text = ScoreManager.Instance.ComputeScore().ToString();
+    }
+}
